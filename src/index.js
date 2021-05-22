e04538edafd547a2ea21fd65c6ee4fd28339b486
TomCSmit <66755579+TomCSmit@users.noreply.github.com>
2021-05-23 00:08:40 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const pizzas = [
     {   pizza_id: 0, 
         name: "margarita", 
         vegetarian: true, 
-        price: 12.50 
+        price: 12.50,
+        toppings: [
+            "Mozzarella",
+            "Tomatoes",
+            "Basil"
+        ]
     },
     {   pizza_id: 1, 
         name: "meatlovers", 
         vegetaria: false, 
-        price: 15.75 
+        price: 15.75,
+        toppings: [
+            "Parmesan cheese",
+            "Pepperoni",
+            "Ham",
+            "Hot sausage",
+            "Tomatoes"
+        ]
     }
 ];
 
@@ -19,35 +33,26 @@ var orders = [
         id: 1,
         customer_id: "123456",
         status: "In Progress",
-        ordered_at: "2021-05-07 08:50:53",
+        ordered_at: "2021-05-22T23:17:53.511Z",
         takeaway: false,
         payment_type: "cash",
         delivery_address: {
-            street: "something",
-            city: "something",
-            country: "something",
-            zipcode: "something" 
+            street: "Paul-Henri Spaaklaan 1",
+            city: "Maastricht",
+            country: "Netherlands",
+            zipcode: "6229 EN" 
         },
         pizzas: [
-            
-                {
-                    pizza_id: 0,
-                    name: "margarita",
-                    vegetarian: true,
-                    price: 12.50
-                },
-                {
-                    pizza_id: 1,
-                    name: "meatlovers",
-                    vegetarian: false,
-                    price: 15.75
-                }
-        ]
+            0,
+            0,
+            1
+        ],
+        note: "No Onions"
     }
 ];
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Pizza API');
 });
 
 app.get('/pizza', (req, res) => {
@@ -56,33 +61,81 @@ app.get('/pizza', (req, res) => {
 
 app.get('/pizza/:id', (req, res) => {
     const pizza = pizzas.find(c => c.pizza_id === parseInt(req.params.id));
-    if (!pizza) res.status(404).send('Pizza not found');
+    if (!pizza) {
+        res.status(404).send('Pizza not found');
+        return;
+    } 
     res.status(200).send(pizza);
 });
 
 app.get('/order/:id', (req, res) => {
     const id = filterInt(req.params.id);
-    if(isNaN(id)) res.status(400).send('Invalid ID supplied');
+    if(isNaN(id)) {
+        res.status(400).send('Invalid ID supplied');
+        return;
+    }
     const order = orders.find(c => c.id === id);
-    if(!order) res.status(404).send('Order ID not found');
+    if(!order) {
+        res.status(404).send('Order ID not found');
+        return;
+    } 
     res.status(200).send(order);
 });
 
-/*app.get('/pizza', (req, res) => {
-    res.status(200).send(pizzas);
+app.get('/order/deliverytime/:id', (req, res) => {
+    const order = orders.find(c => c.id === parseInt(req.params.id));
+    if (!order) {
+        res.status(404).send('Order not found');
+        return;
+    } 
+    order.delivery_time = calculateDeliveryTime(order.ordered_at, 30);
+    res.status(200).send(order);
 });
 
-app.get('/pizza', (req, res) => {
-    res.status(200).send(pizzas);
+app.post('/order', (req, res) => {
+    if (!req.body.pizzas || !req.body.takeaway || !req.body.payment_type || !req.body.customer_id || !req.body.delivery_address) {
+        res.status(400).send("The format of the object is not valid");
+        return;
+    } 
+    const order = {
+        id: orders.length + 1,
+        customer_id: req.body.customer_id,
+        status: "In Progress",
+        ordered_at: getTime(),
+        takeaway: req.body.takeaway,
+        payment_type: req.body.payment_type,
+        delivery_address: req.body.delivery_address,
+        pizzas: req.body.pizzas,
+        note: req.body.note
+    };
+    orders.push(order);
+    order.delivery_time = calculateDeliveryTime(order.ordered_at, 30);
+    res.status(200).send(order);
 });
 
-app.get('/pizza', (req, res) => {
-    res.status(200).send(pizzas);
+app.put('/order/cancel/:id', (req, res) => {
+    const order = orders.find(c => c.id === parseInt(req.params.id));
+    if (!order) {
+        res.status(404).send("Order not found");
+        return;
+    } 
+    if (order.status === "cancelled" || order.status === "delivered") {
+        res.status(422).send("Unable to cancel an already canceled or delivered order");
+        return;
+    } 
+    if (check5MinutesPassed(order.ordered_at)) {
+        res.status(412).send("Unable to cancel your order after 5 minutes have elapsed");
+        return;
+    }
+    order.status = "canceled";
+    const returnOrder = {
+        order_id: order.id,
+        status: "Cancelled"
+    };
+    res.status(200).send(returnOrder);
 });
 
-app.get('/pizza', (req, res) => {
-    res.status(200).send(pizzas);
-});*/
+
 
 //function to filter integers from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt 
 function filterInt(value) {
@@ -91,7 +144,25 @@ function filterInt(value) {
     } else {
       return NaN
     }
-  }
+}
+
+function getTime() {
+    var date = (new Date()).getTimezoneOffset() * 60000;
+    return (new Date(Date.now() - date)).toISOString();
+}
+
+function check5MinutesPassed(orderTimeString) {
+    var orderTime = new Date(orderTimeString).getTime();
+    if (new Date().getTime() - orderTime > 300000) {
+        return true;
+    }
+    return false;
+}
+
+function calculateDeliveryTime(orderTimeString, minutes) {
+    var orderTime = new Date(orderTimeString).getTime();
+    return new Date(orderTime + minutes*60000);
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
